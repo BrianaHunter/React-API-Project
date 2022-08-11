5a6fda26953585216eb119a85b5ddb87ec802a3f
Bianca Gjokaj <bianca.gjokaj@gmail.com>
2022-08-10 21:20:57 -0400

    commented out map function

--- src/component/Movie.tsx
+++ src/component/Movie.tsx
@@ -3,7 +3,7 @@ import { fetchMovieData } from "../services/movies.service";
 import { useEffect, useState } from "react";
 
 export function Movies() {
-  const [movies, setMovies] = useState<FilterMovies[]>();
+  const [movies, setMovies] = useState<FilterMovies[]>([]);
 
   useEffect(() => {
     getAllMovies();
@@ -18,13 +18,13 @@ export function Movies() {
     <div>
       <h1>Movies</h1>
       <ul>
-        {movies?.map((movieList) => (
+        {/* {movies.map((movieList) => (
           <li key={movieList.title}>
             <h2>{movieList.title}</h2>
             <p>{movieList.releaseDate}</p>
             <p>{movieList.voteAverage}</p>
           </li>
-        ))}
+        ))} */}
       </ul>
     </div>
   );
