85ac05b37f3104e61ddc47c46da4be6dc3bfc757
KJ Johnson <kjohns95@emich.edu>
2022-08-11 19:26:19 -0400

    added SearchMovie component and rendered it in the app.tsx - not working currently

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Movies } from "./component/Movie";
 import React from "react";
+import SearchMovie from "./component/SearchMovie";
 
 function App() {
   return (
     <div className="App">
+      <SearchMovie />
       <Movies />
     </div>
   );
